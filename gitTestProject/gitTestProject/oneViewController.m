f2bed94aa8b7a5483c718ea2aba0d1e29c133704
zhuangzhuang <15201162160@163.com>
2019-03-12 15:00:10 +0800

    提交到devone

---
+++ gitTestProject/gitTestProject/oneViewController.m
@@ -0,0 +1,33 @@
+//
+//  oneViewController.m
+//  gitTestProject
+//
+//  Created by xinxinxiangzhao on 2019/3/12.
+//  Copyright © 2019年 杨壮. All rights reserved.
+//
+
+#import "oneViewController.h"
+
+@interface oneViewController ()
+
+@end
+
+@implementation oneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"oneVC");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
