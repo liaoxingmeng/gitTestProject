08d3ab973034f40c6a1b886e6b073c1c59a3e1d8
liaoxingmeng <791153214@qq.com>
2019-03-12 16:10:56 +0800

    移动了文件的存储位置

---
+++ gitTestProject/gitTestProject/testMovePath/twoViewController.h
@@ -0,0 +1,13 @@
+//
+//  twoViewController.h
+//  gitTestProject
+//
+//  Created by yzxin on 2019/3/12.
+//  Copyright © 2019年 杨壮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface twoViewController : UIViewController
+
+@end
---
+++ gitTestProject/gitTestProject/testMovePath/twoViewController.m
@@ -0,0 +1,37 @@
+//
+//  twoViewController.m
+//  gitTestProject
+//
+//  Created by yzxin on 2019/3/12.
+//  Copyright © 2019年 杨壮. All rights reserved.
+//
+
+#import "twoViewController.h"
+
+@interface twoViewController ()
+
+@end
+
+@implementation twoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
